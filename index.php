9ec09f81e0b30458b06b418a6e7c8333a3f247a1
Sergey <52758108+Sergey-life@users.noreply.github.com>
2020-01-18 15:15:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,6 @@ $dsn = "mysql:host=$host;dbname=$db";
 
 $conn = new PDO($dsn, $username, $password);
 
-$result = $conn->query("SELECT * FROM products");
-$product = $result->fetch();
-
 $result = file_get_contents('php://input');
 
 $logString = 'Content received: ' . $result . "\n";
@@ -33,18 +30,13 @@ foreach ($array['product'] as $product) {
 
         $query = $conn->prepare("SELECT * FROM table WHERE unid = :unid, quantity = :quantity, scu = :scu, barcode = :barcode, warehouse = :warehouse, size = :size");
         $result = $query->execute($product);
-        $query->fetchAll();
 
     if ($query->fetch()) {
         $query = ("UPDATE table SET unid = :unid, quantity = :quantity, scu = :scu, barcode = :barcode, warehouse = :warehouse, size = :size WHERE unid = :unid");
         $result = $query->execute($product);
-        $query->fetchAll();
 
     }else {
         $query = $conn->prepare("INSERT INTO products (unid, quantity, scu, barcode, warehouse, size) VALUES (:unid, :quantity, :scu, :barcode, :warehouse, :size)");
         $result = $query->execute($product);
-        $query->fetchAll();
     }
 }
-
-
