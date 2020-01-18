fa93c98b06165a4cb8c918088e7624aabd7b2c43
Serhiy Kharchenko <uetsmedia@UETSMedia-2.local>
2020-01-18 11:34:17 +0200

    Second commit

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
-require_once 'dbconfig.php';
+$host = 'localhost';
+$db = 'product_db';
+$username = 'root';
+$password = 'root';
 
 $key = 'secret_key';
-
 
 if ($_SERVER['HTTP_TOKEN'] != $key) {
     die('Access denied');
@@ -13,7 +15,7 @@ $dsn = "mysql:host=$host;dbname=$db";
 
 $conn = new PDO($dsn, $username, $password);
 
-$result = $conn->query("SELECT * FROM product");
+$result = $conn->query("SELECT * FROM products");
 $product = $result->fetch();
 
 $result = file_get_contents('php://input');
@@ -25,16 +27,16 @@ file_put_contents('log.txt', $logString, FILE_APPEND);
 $array = json_decode($result, true);
 
 foreach ($array['product'] as $product) {
-
+print_r($product);
+//die;
     $exist = $conn->query("SELECT * FROM table WHERE unid = :unid", $product);
     if ($exist->fetch()) {
         //Do update data
         $conn->query("UPDATE table SET unid = :unid, quantity = :quantity, scu = :scu, barcode = :barcode, warehouse = :warehouse, size = :size WHERE unid = :unid", $product);
 
     }else {
-        $conn->query("INSERT INTO product (unid, quantity,scu, barcode, warehouse, size) VALUES (:unid, :quantity, :scu, :barcode, :warehouse, size)", $product );
+        $conn->query("INSERT INTO products (unid, quantity,scu, barcode, warehouse, size) VALUES (:unid, :quantity, :scu, :barcode, :warehouse, :size)", $product );
     }
-
 }
 
 
